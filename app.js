c57b89d7a9049b73f5c70d37989f427e8d69ac54
Samuel <sammyokorie0@gmail.com>
2022-06-21 19:01:39 +0100

    npm installed ejs, used ejs format, created add account page, removed content from index.html and split them into header.ejs, home.ejs and added content into add-account ejs page

--- app.js
+++ app.js
@@ -2,14 +2,25 @@ const express = require("express");
 const bodyParser = require("body-parser");
 
 const app = express();
-app.set('views', 'ejs');
+app.set("view engine", "ejs");
 app.use(express.static('public'));
 
 app.use(bodyParser.urlencoded({extended: true}));
 
 app.get("/", function(req, res){
-  res.sendFile(__dirname + "/index.html");
+  res.render("home");
 })
+
+app.get("/add-account", function(req, res){
+  res.render("add-account");
+})
+
+
+
+
+
+
+
 
 app.listen("3000", function(){
   console.log("Server is running on port 3000");
